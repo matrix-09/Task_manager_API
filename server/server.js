17496ed38b4c7bd88e399fe7e69906712637e4ce
mahideep <mahideepnagireddi@gmail.com>
2025-04-28 23:33:01 +0530

    Your message, e.g., 'Fixed vulnerabilities and updated packages'

--- server/server.js
+++ server/server.js
@@ -31,5 +31,5 @@ app.use('/api/users', require('./routes/userRoutes'));
 app.use(require('./middleware/error'));
 
 // Start server
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5000
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
